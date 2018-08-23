0bb1432ebc9913962a393b86b8d9ea6ec04bdea3
BlueCompany <gronczewskim@gmail.com>
2018-08-23 21:26:43 +0200

    a

--- JIPP_AM/Form1.cs
+++ JIPP_AM/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,79 @@ namespace JIPP_AM
 {
     public partial class Form1 : Form
     {
+        int dx;
+        int dy;
+        int dxDest;
+        int dyDest;
+        System.Timers.Timer timer;
+        delegate void ReachDestination();
+        event ReachDestination DestinationReached;
+        int pictureWidth = 100;
+        int pictureHeight = 100;
         public Form1()
         {
             InitializeComponent();
+            //timer, jesli wlaczony bedzie trigerowal zdarzenie elapsed co 30 ms
+            timer = new System.Timers.Timer(30);
+            //dopisuje metode do zdarzenia
+            timer.Elapsed += Timer_Elapsed;
+            //dopisuje do wlasnego zdarzenia jakim jest DestinationReached metode lambda ktora wylaczy mi stoper
+            //wyrazenie lambda mozna traktowac jako metode anonimowa
+            //alternatywa dla tego zapisu byloby zdefiniowanie funkcji ktora zwraca void i nie przyjmuje zadnych argumentow
+            DestinationReached += (() => 
+            {
+                timer.Enabled = false;
+            });
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            MoveToTheDirection();
+            //sprawdzamy czy powinnismy wywolac zdarzenie DestinationReached
+            if (dx == dxDest && dy == dyDest)
+            {
+                DestinationReached();
+            }
+        }
+        //metoda wywolywana przy wcisnieciu myszki
+        private void tableLayoutPanel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine(e.Location.X.ToString()+" "+e.Location.Y.ToString());
+            //pobieram lokalizacje gdzie zostala wcisnieta myszka
+            //odejmowanie ma spowodowac, ze obrazek poruszy sie w swoje centrum
+            dxDest = e.Location.X - pictureWidth/2;
+            dyDest = e.Location.Y - pictureHeight/2;
+            timer.Enabled = true;
+        }
+        private void MoveToTheDirection()
+        {
+            if (dx < dxDest)
+            {
+                dx++;
+            }
+            else if (dx > dxDest)
+            {
+                dx--;
+            }
+            if (dy < dyDest)
+            {
+                dy++;
+            }
+            else if (dy > dyDest)
+            {
+                dy--;
+            }
+            Draw();
+        }
+        void Draw()
+        {
+            //rysowanie
+            Graphics graphics = tableLayoutPanel1.CreateGraphics();
+            graphics.Clear(tableLayoutPanel1.BackColor);
+            //TRZEBA PODMIENIC SCIEZKE
+            Image image = Image.FromFile(@"C:\Users\BlueCompany\source\repos\JIPP_AM\JIPP_AM\person.png");
+            var bmp = new Bitmap(image, new Size(pictureWidth, pictureHeight));
+            graphics.DrawImage(bmp, dx, dy);
         }
     }
 }
